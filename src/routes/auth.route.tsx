6b03ef3ea10a0adafd5bff6fb187ea12aafc23c8
Delight Aberage <delightvincent487@gmail.com>
2026-02-15 10:35:52 +0100

    cleaned the auth route file

--- src/routes/auth.route.tsx
+++ src/routes/auth.route.tsx
@@ -1,12 +1,5 @@
-import { render } from '@react-email/components';
 
 import { Router } from "express";
-import { pool } from "../db/db.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { JwtPayload, User } from "../types/authType.js";
-import SendEmailUi from '../utils/react-email-starter/emails/SendEmail.js';
-import SendEmail from '../lib/sendEmail.js';
 import { forgotPasswordController, hydration, loginController,  RefreshTokenController,  ResetPasswordController, signupController } from '../controllers/auth.controller.js';
 
 
