e8f858db9358ecc48ce68e1da5fa718a46663068
Delight Aberage <delightvincent487@gmail.com>
2026-02-19 13:55:14 +0100

    push update

--- src/repository/product.repositroy.ts
+++ src/repository/product.repositroy.ts
@@ -1,21 +1,20 @@
 import { pool } from "../db/db";
 import { ProductType } from "../types/product";
-import { fetchProduct } from "../utils/product";
 
 
 
 
 
-  export const InsertProduct = async () => {
+
+  export const InsertProduct = async (product: ProductType) => {
         try {
-             const product: ProductType = await fetchProduct();
-             console.log(product);
-            
-             const InsertProduct = await pool.query('INSERT INTO products (product_name, price, description, category,  image_url, rating) VALUES($1, $2, $3, $4, $5)'[product.title, product.price, product.description, product.image, product.rating])
-        } catch (error) {
+             const InsertProduct = await pool.query('INSERT INTO products (product_name, price, description, category,  image_url, rating) VALUES($1, $2, $3, $4, $5) RETURNING*',
+                [product.title, product.price, product.description, product.image, product.rating])
+           return InsertProduct.rows[0];
+            } catch (error) {
             return console.log(error);
         
         }
   }
 
-
+ 
--- src/utils/product.ts
+++ src/utils/product.ts
@@ -3,10 +3,12 @@ import  axios  from 'axios';
 
 export const fetchProduct = async () => {
     try {
-        const products = axios.get('https://fakestoreapi.com/products');
+        const products = await axios.get('https://fakestoreapi.com/products');
         return products;
     } catch (error) {
        console.log(error);
         return error;
     }
 }
+
+fetchProduct()
